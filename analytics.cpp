0d4770a54ad9f8b064a3ffa1c92c85b51ff9e8e6
Leo-Leonis <yeleandro@icloud.com>
2023-03-25 18:19:23 +0100

    Finito progetto
    In teoria ho completato il progetto. Sistemato canvas, aggiunto output file, aggiunto c o s m e t i c a, finito di bere

--- analytics.cpp
+++ analytics.cpp
@@ -43,24 +43,17 @@ void check_entries(TH1 *histo, int expEntries, int range) {
 /// @param histo the histogram pointer
 /// @param bin the number associated to the bin
 /// @param expEntries the number of expected entries
-/// @param range the accepted error range
-void check_bin_entries(TH1 *histo, int bin, int expEntries, int range) {
+void check_bin_entries(TH1 *histo, int bin, int expEntries) {
   int entries = histo->GetBinContent(bin);
-  std::cout << "INFO: checking " << histo->GetName() << ", bin " << bin
-            << "... ";
-  if (entries >= expEntries - range && entries <= expEntries + range) {
-    std::cout << "correct! (" << entries << ')' << '\n';
-  } else {
-    std::cout << "value out of range! (" << entries
-              << "; expected: " << expEntries << ')' << '\n';
-  }
+  std::cout << "INFO: checking " << histo->GetName() << ", bin " << bin << ": "
+            << entries << " entries (expected: " << expEntries << ')' << '\n';
 }
 
 void analysis() {
 
   gStyle->SetOptStat(2200);
   gStyle->SetOptFit(1111);
-  
+
   TFile *resultFile = new TFile("result.root");
 
   std::cout << '\n';
@@ -92,26 +85,32 @@ void analysis() {
 
   std::cout << "\n\n";
 
+  /////////////////////////////////1/4//////////////////////////////////////////
   std::cout << "CHECKING PARTICLE TYPES GENERATION------------------------\n";
-  check_bin_entries(hIndex, 1, 4E6, 1E5);    // pi+ (40%)
-  check_bin_entries(hIndex, 2, 4E6, 1E5);    // pi- (40%)
-  check_bin_entries(hIndex, 3, 5E5, 1E3);    // K+ (5%)
-  check_bin_entries(hIndex, 4, 5E5, 1E3);    // K- (5%)
-  check_bin_entries(hIndex, 5, 450000, 1E3); // p+ (4.5%)
-  check_bin_entries(hIndex, 6, 450000, 1E3); // p- (4.5%)
-  check_bin_entries(hIndex, 7, 1E5, 1E3);    // K* (1%)
+  check_bin_entries(hIndex, 1, 4E6);    // pi+ (40%)
+  check_bin_entries(hIndex, 2, 4E6);    // pi- (40%)
+  check_bin_entries(hIndex, 3, 5E5);    // K+ (5%)
+  check_bin_entries(hIndex, 4, 5E5);    // K- (5%)
+  check_bin_entries(hIndex, 5, 450000); // p+ (4.5%)
+  check_bin_entries(hIndex, 6, 450000); // p- (4.5%)
+  check_bin_entries(hIndex, 7, 1E5);    // K* (1%)
 
   std::cout << "\n\n";
 
+  ///////////////////////////////2/4//////////////////////////////////////////
   std::cout << "FITTING AND CHECKING ANGLE DISTRIBUTIONS------------------\n";
-  
-  TCanvas *canvas1 =
-      new TCanvas("c1", "Angle distribution", 1080, 720);
-  canvas1->Divide(1,2);
+
+  TCanvas *canvas1 = new TCanvas("c1", "Canvas 1", 1440, 720);
+  canvas1->Divide(2, 2);
   canvas1->cd(1);
+  hIndex->GetXaxis()->SetTitle("Particle Type");
+  hIndex->GetYaxis()->SetTitle("Entries");
+  hIndex->Draw("H");
+  hIndex->Draw("E, P, SAME");
+  canvas1->cd(2);
 
   TF1 *fPhi = new TF1("fPhi", "pol 0", 0, TMath::TwoPi());
-  hPhi->Fit(fPhi, "S, Q"); // "0" for no plotting, "Q" for quiet mode
+  hPhi->Fit(fPhi, "S, Q");
   TF1 *fTheta = new TF1("fTheta", "pol 0", 0, TMath::Pi());
   hTheta->Fit(fTheta, "S, Q");
 
@@ -119,6 +118,8 @@ void analysis() {
   std::cout << "INFO: Phi fit results..." << '\n';
   std::cout << "           Value: " << fitPhiRes->GetParameter(0) << " ± "
             << fitPhiRes->GetParError(0) << '\n'
+            << "           Chi^2: " << fitPhiRes->GetChisquare() << '\n'
+            << "             NDF: " << fitPhiRes->GetNDF() << '\n'
             << "       Chi^2/NDF: "
             << fitPhiRes->GetChisquare() / fitPhiRes->GetNDF() << '\n'
             << "     Probability: " << fitPhiRes->GetProb() << '\n'
@@ -128,32 +129,39 @@ void analysis() {
   std::cout << "INFO: Theta fit results..." << '\n';
   std::cout << "           Value: " << fitThetaRes->GetParameter(0) << " ± "
             << fitThetaRes->GetParError(0) << '\n'
+            << "           Chi^2: " << fitThetaRes->GetChisquare() << '\n'
+            << "             NDF: " << fitThetaRes->GetNDF() << '\n'
             << "       Chi^2/NDF: "
             << fitThetaRes->GetChisquare() / fitThetaRes->GetNDF() << '\n'
             << "     Probability: " << fitThetaRes->GetProb() << '\n';
 
   std::cout << "\n\n";
 
+  hPhi->GetXaxis()->SetTitle("Angle [rad]");
+  hPhi->GetYaxis()->SetTitle("Entries");
   hPhi->Draw("H");
   hPhi->Draw("E, P, SAME");
-  canvas1->cd(2);
+
+  canvas1->cd(3);
+  hTheta->GetXaxis()->SetTitle("Angle [rad]");
+  hTheta->GetYaxis()->SetTitle("Entries");
   hTheta->Draw("H");
   hTheta->Draw("E, P, SAME");
 
+  ///////////////////////////////3/4//////////////////////////////////////////
   std::cout << "FITTING AND CHECKING IMPULSE DISTRIBUTION-----------------\n";
-  
-  TCanvas *canvas2 =
-      new TCanvas("c2", "Impulse distribution", 1080, 360);
 
+  canvas1->cd(4);
   TF1 *fImpModule = new TF1("fImpModule", "expo", 0, 6);
   hImpModule->Fit(fImpModule, "S, Q");
-
 
   TF1 *fitImpModuleRes = hImpModule->GetFunction("fImpModule");
 
   std::cout << "INFO: Impulse module fit results..." << '\n';
   std::cout << "           Value: " << -fitImpModuleRes->GetParameter(1)
             << " ± " << fitImpModuleRes->GetParError(1) << '\n'
+            << "           Chi^2: " << fitImpModuleRes->GetChisquare() << '\n'
+            << "             NDF: " << fitImpModuleRes->GetNDF() << '\n'
             << "       Chi^2/NDF: "
             << fitImpModuleRes->GetChisquare() / fitImpModuleRes->GetNDF()
             << '\n'
@@ -161,15 +169,22 @@ void analysis() {
 
   std::cout << "\n\n";
 
+  hImpModule->GetXaxis()->SetTitle("Impulse [GeV]");
+  hImpModule->GetYaxis()->SetTitle("Entries");
   hImpModule->Draw("H");
   hImpModule->Draw("E, P, SAME");
 
+  canvas1->Print("canvas1.png");
+  canvas1->Print("canvas1.C");
+  canvas1->Print("canvas1.root");
+
+  ///////////////////////////////4/4//////////////////////////////////////////
   std::cout << "CHECKING INVARIANT MASS HISTOGRAMS------------------------\n";
-  
-  TCanvas *canvas3 =
-      new TCanvas("c3", "Difference invariant mass histograms", 1080, 720);
-  canvas3->Divide(1, 2);
-  canvas3->cd(1);
+
+  TCanvas *canvas2 =
+      new TCanvas("c2", "Difference invariant mass histograms", 720, 720);
+  canvas2->Divide(1, 3);
+  canvas2->cd(1);
 
   TH1F *hSubAll = new TH1F("hSubAll",
                            "Difference between opposite charge particles from "
@@ -195,6 +210,10 @@ void analysis() {
             << fitSubAllRes->GetParError(1) << '\n'
             << "   K* Width (SD): " << fitSubAllRes->GetParameter(2) << " ± "
             << fitSubAllRes->GetParError(2) << '\n'
+            << "      gaus width: " << fitSubAllRes->GetParameter(0) << " ± "
+            << fitSubAllRes->GetParError(0) << '\n'
+            << "           Chi^2: " << fitSubAllRes->GetChisquare() << '\n'
+            << "             NDF: " << fitSubAllRes->GetNDF() << '\n'
             << "       Chi^2/NDF: "
             << fitSubAllRes->GetChisquare() / fitSubAllRes->GetNDF() << '\n'
             << "     Probability: " << fitSubAllRes->GetProb() << '\n'
@@ -207,19 +226,34 @@ void analysis() {
             << fitSubKpiRes->GetParError(1) << '\n'
             << "   K* Width (SD): " << fitSubKpiRes->GetParameter(2) << " ± "
             << fitSubKpiRes->GetParError(2) << '\n'
+            << "      gaus width: " << fitSubKpiRes->GetParameter(0) << " ± "
+            << fitSubKpiRes->GetParError(0) << '\n'
+            << "           Chi^2: " << fitSubKpiRes->GetChisquare() << '\n'
+            << "             NDF: " << fitSubKpiRes->GetNDF() << '\n'
             << "       Chi^2/NDF: "
             << fitSubKpiRes->GetChisquare() / fitSubKpiRes->GetNDF() << '\n'
             << "     Probability: " << fitSubKpiRes->GetProb() << '\n'
             << '\n';
 
-  canvas3->cd(1);
+  canvas2->cd(1);
+  hSubAll->GetXaxis()->SetTitle("Invariant Mass [GeV/c^2]");
+  hSubAll->GetYaxis()->SetTitle("Entries");
   hSubAll->Draw("H");
   hSubAll->Draw("E, P, SAME");
-  canvas3->cd(2);
+
+  canvas2->cd(2);
+  hSubKpi->GetXaxis()->SetTitle("Invariant Mass [GeV/c^2]");
+  hSubKpi->GetYaxis()->SetTitle("Entries");
   hSubKpi->Draw("H");
   hSubKpi->Draw("E, P, SAME");
 
-  TCanvas *canvas4 =
-      new TCanvas("c4", "Invariant Mass control histogram", 1080, 360);
-  hInvMassControl->Draw();
+  canvas2->cd(3);
+  hInvMassControl->GetXaxis()->SetTitle("Invariant Mass [GeV/c^2]");
+  hInvMassControl->GetYaxis()->SetTitle("Entries");
+  hInvMassControl->Draw("H");
+  hInvMassControl->Draw("E, P, SAME");
+
+  canvas2->Print("canvas2.png");
+  canvas2->Print("canvas2.C");
+  canvas2->Print("canvas2.root");
 }
